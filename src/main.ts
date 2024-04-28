794ab79692682e16b57459507c1085ebb8566075
saber09130605 <1031703364@qq.com>
2024-04-28 23:36:42 +0800

    feat:导航条样式功能修改

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia';
 import router from './router/router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 const app = createApp(App)
 const pinia = createPinia();
 app.use(ElementPlus)
